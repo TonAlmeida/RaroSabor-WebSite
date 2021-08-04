6f9df81b640b498d47b0fdeebfc514a4bbfbf5c8
TonAlmeida <eltonsantos.almeida@outlook.com>
2021-08-04 15:18:18 -0300

    first commit

---
+++ js/myJs.js
@@ -0,0 +1,10 @@
+const linkLogo = document.getElementById('link-home-logo')
+const logo = document.getElementById('home-logo')
+window.addEventListener('scroll', e => {
+    if(scrollY === 0) {
+        logo.style.width = '200px'
+    } else {
+        logo.style.width = '100px'
+    }
+})
+console.log('here')
